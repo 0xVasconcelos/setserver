c2c45a80cb1a5f38f012b45875703258a7a3c37b
Lucas Vasconcelos <lucaslg26@yahoo.com.br>
2016-05-09 10:29:39 -0300

    Alguns comments

--- app.js
+++ app.js
@@ -1,34 +1,61 @@
-var net = require('net');
-var chalk = require('chalk');
-var log = require('./lib/log.js');
-var Sequelize = require('sequelize');
+var net = require('net');                       //Biblioteca para iniciar os sockets TCP
+var chalk = require('chalk');                   //Biblioteca para colorir a saída no console
+var log = require('./lib/log.js');              //Importa o arquivo log para algumas funções
+var Sequelize = require('sequelize');           //Biblioteca para ORM em MySQL
+//Inicia conexão com o DB MySQL através do Sequelize
 var sequelize = new Sequelize('mysql://root:$setufpa@localhost:3306/nodefinger', { logging: false } );
-var config = {};
+/* Configuração do cliente que vai ser conectado 
+    `config.port` é a porta que o servidor TCP será iniciado
+    `config.hwid` é o id de identificação do hardware do SET FINGER
+    `config.name` é o nome que será apresentado na tela de boas-vindas do SET FINGER
+*/
+var config = {};    
     config.port = 7000;
     config.hwid = 'GT-SET';
     config.name = 'GT-SET';
-
+/* Aqui é iniciado o servidor TCP na porta determinada em `config.port`, a função que será executada para cada conexão
+    feita ao servidor será a função `setServer`, ela ficará responsável pelo tratamento dos sockets criados com o servidor TCP.
+*/
 var fingerServer = net.createServer(setServer)
+    /* Evento disparado quando o servidor está sendo iniciado */
     .listen(config.port, () => log('server', "O servidor está sendo iniciado..."))
+    /* Evento disparado quando uma nova conexão é estabelecida com o servidor TCP */
     .on('connection', (data) => log('TCP', 'Conexão estabelecida de ' + chalk.underline(data.remoteAddress + ':' + data.remotePort)))
+    /* Evento disparado quando o servidor é iniciado com sucesso na porta especificada */
     .on('listening', (data) => log('server', 'O servidor foi iniciado em ' + chalk.underline(fingerServer.address().address + ':' + fingerServer.address().port)))
+    /* Evento disparado quando acontece algum erro no servidor TCP, o error é tratado pela função `errorHandler` */
     .on('error', (err) => errorHandler(err));
 
+/* Função que trata os sockets TCP, iniciados por fingerServer */
 function setServer(sock){
+    /* Variável que armazena o id de hardware do SE TFINGER*/
     var sessionId;
+    /* Variável que armazena o último ID que foi passado ao SET FINGER para fins de registro de novo usuário*/
     var lastID;
+    /* Ativa o keepAlive no socket para que o gateway da rede não mate a conexão por inatividade */
     sock.setKeepAlive(true, 30000)
+    /* Evento disparado quando algum dado é recebido, ele é tratado pela função `dataParser` */
     .on('data', (data) => dataParser(data, sock))
+    /* Evento disparado quando alguma conexão é fechada, tratada pela função `errorHandler` */
     .on('close', (data) => errorHandler(data))
+    /* Evento disparado quando acontece algum erro no socket, tratada pela função `errorHandler`*/
     .on('error', (err) => errorHandler(err));
 }
 
 function dataParser(data, sock){
+    /* Como os dados enviados pelo SET FINGER estão no formato JSON, ele tenta tratar esses dados, caso não consiga, 
+    é disparado um erro, que é tratado pela função `errorHandler`
+    Caso o dado seja tratado com sucesso, ele então é convertido de string JSON para um objeto do JavaScript
+    */
     try{
        data = JSON.parse(data);
     } catch(err){
         errorHandler(err);
     }
+    /* Caso o tipo da mensagem seja do tipo 'conn', indica que é um cliente tentando se conectar ao servidor
+       Se o hwid da mensagem for válido(igual ao configurado em config.hwid) ele está autorizado, então o hwid é atribuido
+       ao sessionId.
+    */
     if(data.type === 'conn'){
         log('client', 'O cliente \"' + data.hwid + '\" está tentando se conectar');
         if(data.hwid == config.hwid){
@@ -37,17 +64,28 @@ function dataParser(data, sock){
             log('server', 'O cliente \"' + data.hwid + '\" foi autenticado com sucesso!');
         }
     }
+    /* Se sessionId estiver definido, ele então trata as próximas mensagens que serão enviadas pelo SET FINGER*/
     if(sessionId){
         switch(data.type){
+            /* Quando o SET FINGER tenta autorizar algum cliente ele envia o tipo `fingerid` que é tratado pela função
+            sendMessage */
             case 'fingerid':
                 sendMessage(sock, 'fingerid', data.id)
                 break;
+            /* Quando o SET FINGER tenta cadastrar um novo cliente ele envia o tipo `fingerid` que é tratado pela função
+            sendMessage */
             case 'addfinger':
                 sendMessage(sock, 'addfinger')
                 break;
+            /* Quando o novo usuário foi cadastrado com sucesso no sensor biométrico, ele manda uma mensagem de sucesso,
+                então o servidor armazena no banco o ID que foi passado ao SET FINGER
+            */
             case 'registerok':
                 saveUser(lastID);
                 break;
+            /*
+                Quando algum erro acontece no registro do novo usuário no sensor biométrico
+            */
             case 'registerfail':
                 log('server', 'Falha na tentativa de registro do novo usuário! ID: ' + lastID);
                 break;
